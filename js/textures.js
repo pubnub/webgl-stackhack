1d2d348984de08d3e52bd70769b6a569a3fec480
dristic <dristic101@gmail.com>
2014-02-27 12:13:24 -0800

    Get geometries merged by material id. Add support for creating new blocks in a chunk.

---
+++ js/textures.js
@@ -0,0 +1,91 @@
+var textures = [
+  // Grass cube
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/grass_side.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/grass_side.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/grass_top.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/grass_side.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/grass_side.png")
+  ], 
+
+  // Oak block
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak_top.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/log_big_oak.png")
+  ],
+
+  // Oak Leaves block
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/leaves_big_oak_opaque.png")
+  ],
+
+  // Dirt
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/dirt.png")
+  ],
+
+  // Sand
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/sand.png")
+  ],
+
+  // Snow
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/snow.png")
+  ],
+
+  // Ice
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/ice.png")
+  ],
+
+  // Furnace
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_front_on.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_side.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_top.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_top.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_side.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/furnace_side.png")
+  ],
+
+  // Cobblestone
+  [
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png"),
+    THREE.ImageUtils.loadTexture("textures/blocks/cobblestone.png")
+  ],
+];
